f75dde7fc41de64f63f28051c918764b91363215
IgorCarvlho <igorshadow15@gmail.com>
2023-06-03 16:27:36 -0300

    Finished People Details

--- src/screens/PeoplesDetails/index.tsx
+++ src/screens/PeoplesDetails/index.tsx
@@ -1,17 +1,226 @@
+import { useEffect, useState } from "react";
+import { ActivityIndicator } from "react-native";
 import { useRoute } from "@react-navigation/native";
+
+import api from "../../services/api";
 
 import Header from "../../components/Header";
 
-import { ContainerPeoplesDetails } from "./styles";
+import {
+  ContainerDetail,
+  ContainerDetails,
+  ContainerInformation,
+  ContainerLoading,
+  ContainerPeoplesDetails,
+  TextDetail,
+  TitleDetail,
+} from "./styles";
 
 export default function PeoplesDetails() {
   const route = useRoute<any>();
 
   const { details } = route.params;
 
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [films, setFilms] = useState([]);
+  const [homeworlds, setHomeworlds] = useState();
+  const [species, setSpecies] = useState([]);
+  const [vehicles, setVehicles] = useState([]);
+  const [starships, setStarships] = useState([]);
+
+  function getHomeworlds() {
+    return api.get(details?.homeworld).then((res) => {
+      setHomeworlds(res.data.name);
+    });
+  }
+
+  async function getFilmTitle() {
+    try {
+      const urls = details?.films;
+      const filmsTitle: any = [];
+
+      for (const url of urls) {
+        const { data } = await api.get(url);
+        filmsTitle.push(data.title);
+      }
+
+      setFilms(filmsTitle);
+    } catch (error) {
+      console.error("Ocorreu um erro ao obter os títulos dos filmes:", error);
+    }
+  }
+
+  async function getSpeciesTitle() {
+    try {
+      const urls = details?.species;
+      const speciesTitle: any = [];
+
+      for (const url of urls) {
+        const { data } = await api.get(url);
+        speciesTitle.push(data.name);
+      }
+
+      setSpecies(speciesTitle);
+    } catch (error) {
+      console.error("Ocorreu um erro ao obter os títulos das espécies:", error);
+    }
+  }
+
+  async function getVehiclesTitle() {
+    try {
+      const urls = details?.vehicles;
+      const vehiclesTitle: any = [];
+
+      for (const url of urls) {
+        const { data } = await api.get(url);
+        vehiclesTitle.push(data.name);
+      }
+
+      setVehicles(vehiclesTitle);
+    } catch (error) {
+      console.error("Ocorreu um erro ao obter os títulos dos veículos:", error);
+    }
+  }
+
+  async function getStarshipsTitle() {
+    try {
+      const urls = details?.starships;
+      const starshipsTitle: any = [];
+
+      for (const url of urls) {
+        const { data } = await api.get(url);
+        starshipsTitle.push(data.name);
+      }
+
+      setStarships(starshipsTitle);
+    } catch (error) {
+      console.error(
+        "Ocorreu um erro ao obter os títulos das naves estelares:",
+        error
+      );
+    }
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setIsLoading(true);
+
+        await Promise.all([
+          getHomeworlds(),
+          getFilmTitle(),
+          getSpeciesTitle(),
+          getVehiclesTitle(),
+          getStarshipsTitle(),
+        ]);
+
+        setIsLoading(false);
+      } catch (error) {
+        console.error(error);
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <ContainerPeoplesDetails>
       <Header title="Detalhes" />
+      <ContainerDetails>
+        <ContainerDetail>
+          <TitleDetail>Nome:</TitleDetail>
+          <TextDetail>{details?.name}</TextDetail>
+        </ContainerDetail>
+
+        <ContainerDetail>
+          <TitleDetail>Altura:</TitleDetail>
+          <TextDetail>{details?.height} cm</TextDetail>
+        </ContainerDetail>
+
+        <ContainerDetail>
+          <TitleDetail>Cor dos Olhos:</TitleDetail>
+          <TextDetail>{details?.eye_color}</TextDetail>
+        </ContainerDetail>
+
+        <ContainerDetail>
+          <TitleDetail>Cor da Pele:</TitleDetail>
+          <TextDetail>{details?.skin_color}</TextDetail>
+        </ContainerDetail>
+
+        <ContainerDetail>
+          <TitleDetail>Cor do Cabelo:</TitleDetail>
+          <TextDetail>{details?.hair_color}</TextDetail>
+        </ContainerDetail>
+
+        <ContainerDetail>
+          <TitleDetail>Ano de Nascimento:</TitleDetail>
+          <TextDetail>{details?.birth_year}</TextDetail>
+        </ContainerDetail>
+
+        <ContainerDetail>
+          <TitleDetail>Gênero:</TitleDetail>
+          <TextDetail>{details?.gender}</TextDetail>
+        </ContainerDetail>
+
+        <ContainerDetail>
+          <TitleDetail>Planeta Natal:</TitleDetail>
+          <TextDetail>{homeworlds}</TextDetail>
+        </ContainerDetail>
+      </ContainerDetails>
+
+      {isLoading ? (
+        <ContainerLoading>
+          <ActivityIndicator size="large" color="white" />
+        </ContainerLoading>
+      ) : (
+        <>
+          {films[0] && (
+            <ContainerInformation>
+              <TitleDetail>Filmes:</TitleDetail>
+              <ContainerDetail>
+                {films.map((film) => (
+                  <TextDetail key={film}>{film}</TextDetail>
+                ))}
+              </ContainerDetail>
+            </ContainerInformation>
+          )}
+
+          {species[0] && (
+            <ContainerInformation>
+              <TitleDetail>Espécies:</TitleDetail>
+              <ContainerDetail>
+                {species.map((specie) => (
+                  <TextDetail key={specie}>{specie}</TextDetail>
+                ))}
+              </ContainerDetail>
+            </ContainerInformation>
+          )}
+
+          {vehicles[0] && (
+            <ContainerInformation>
+              <TitleDetail>Veículos:</TitleDetail>
+              <ContainerDetail>
+                {vehicles.map((vehicle) => (
+                  <TextDetail key={vehicle}>{vehicle}</TextDetail>
+                ))}
+              </ContainerDetail>
+            </ContainerInformation>
+          )}
+
+          {starships[0] && (
+            <ContainerInformation>
+              <TitleDetail>Naves Estelares:</TitleDetail>
+              <ContainerDetail>
+                {starships.map((starship) => (
+                  <TextDetail key={starship}>{starship}</TextDetail>
+                ))}
+              </ContainerDetail>
+            </ContainerInformation>
+          )}
+        </>
+      )}
     </ContainerPeoplesDetails>
   );
 }
--- src/screens/PeoplesDetails/styles.ts
+++ src/screens/PeoplesDetails/styles.ts
@@ -9,3 +9,49 @@ export const ContainerPeoplesDetails = styled.View`
 
   background-color: ${({ theme }) => theme.colors.background};
 `;
+
+export const ContainerDetails = styled.View`
+  width: 100%;
+
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-between;
+  gap: 16px;
+
+  margin-top: 10%;
+  margin-bottom: 15%;
+`;
+
+export const ContainerDetail = styled.View`
+  width: 45%;
+`;
+
+export const TitleDetail = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.medium};
+  font-size: 16px;
+  color: ${({ theme }) => theme.colors.white};
+`;
+
+export const TextDetail = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: 16px;
+  color: ${({ theme }) => theme.colors.white};
+  text-transform: capitalize;
+
+  padding-top: 4px;
+  padding-left: 24px;
+`;
+
+export const ContainerInformation = styled.View`
+  width: 100%;
+
+  margin-top: 12px;
+`;
+
+export const ContainerLoading = styled.View`
+  width: 100%;
+
+  align-items: center;
+  justify-content: center;
+`;
