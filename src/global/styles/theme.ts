d660a5283f13425e15332e2a94b55912c7a11cea
IgorCarvlho <igorshadow15@gmail.com>
2023-06-02 23:13:28 -0300

    Added theme

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,17 @@
+export default {
+  colors: {
+    background: "#121215",
+    background_secundary: "#e60c0d",
+
+    white: "#FFFFFF",
+    light: "#e5e5e5",
+    dark: "#161616",
+    black: "#000000",
+  },
+
+  fonts: {
+    regular: "Roboto_400Regular",
+    medium: "Roboto_500Medium",
+    bold: "Roboto_700Bold",
+  },
+};
